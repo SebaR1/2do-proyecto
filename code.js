7eaf809409b6e7048ebc5294e2829650551f251e
Sebastian Romero <romerosebastian781@gmail.com>
2022-10-31 05:52:24 -0300

    actualizacion2

--- code.js
+++ code.js
@@ -1,3 +1,157 @@
+/*
+//1ra clase Coderhouse 
+let num1 = parseInt(prompt("Ingresa un numero"));
+let num2 = parseInt(prompt("Ingresa otro num"));
+let suma = num1 + num2;
+
+alert("La suma de ambos numeros es= " + suma);
+
+//2da clase Coderhouse
+let nombre;
+let contraseña;
+alert("Proceso para registrarse");
+let aux1 = prompt("Ingrese un nick");
+if (aux1.length >= 5) {
+  nombre = aux1;
+} else {
+  aux1 = prompt("Ingrese otro nick de más de 5 caracteres");
+  nombre = aux1;
+}
+let aux2 = prompt("Ingrese su contraseña");
+if (aux2.length >= 10) {
+  contraseña = aux2;
+} else {
+  aux2 = prompt("Ingrese otra contraseña de más de 10 caracteres");
+  contraseña = aux2;
+}
+
+alert("Proceso para ingresar");
+let aux3 = prompt("Ingrese su nick");
+let aux4 = prompt("Ingrese su contraseña");
+
+if (aux3 == nombre && aux4 == contraseña) {
+  alert("Bienvenido " + nombre + "!");
+} else if (aux3 == nombre && aux4 != contraseña) {
+  alert("El usuario esta bien, pero esta mal la contraseña");
+} else if (aux4 != nombre && aux4 != contraseña) {
+  alert(
+    "Ni el usuario ni la contraseña esta registrado en nuestros datos, la policia esta en camino a su domicilio"
+  );
+}
+
+
+//3ra clase Coderhouse
+
+let suma = 0;
+
+for (let i = 1; i <= 5; i++) {
+  let num = parseInt(prompt("Digite el numero " + i));
+  suma = suma + num;
+}
+
+alert("La suma de los 5 numeros fue " + suma);
+
+
+
+let sum = 0;
+for (let i = 1; i <= 3; i++) {
+  let notas = parseInt(prompt("Ingrese su nota numero " + i));
+  sum = sum + notas;
+}
+let prom = sum / 3;
+if (prom >= 7) {
+  alert("Felicidades! Aprobaste la materia, tu promedio fue de " + prom);
+} else {
+  alert(
+    "Lamentablemente no alcanzo, tu promedio fue de " +
+      prom +
+      ". Exitos para la proxima!"
+  );
+}
+
+//1ER ENTREGA
+
+function pediropc() {
+  opc = parseInt(
+    prompt(
+      "Seleccione una opcion(un numero).\n1.Para saber con cuanto efectivo contas\n2.Ingresar dinero\n3.Retirar dinero.\nSi desea salir presione otro número"
+    )
+  );
+}
+
+function mostrardinero() {
+  alert("En la cuenta tienes " + dinero);
+}
+
+function menu() {
+  while (opc == 1 || opc == 2 || opc == 3) {
+    switch (opc) {
+      case 1:
+        mostrardinero();
+        pedirvolver();
+        break;
+      case 2:
+        let dineroadepositar = parseInt(
+          prompt("Ingrese la cantidad a depositar")
+        );
+        if (
+          dineroadepositar > 0 &&
+          !isNaN(dineroadepositar) &&
+          dineroadepositar != ""
+        ) {
+          dinero = dinero + dineroadepositar;
+        } else {
+          alert("Ingrese bien la cantidad!");
+        }
+        mostrardinero();
+        pedirvolver();
+        break;
+      case 3:
+        let dineroaretirar = parseInt(prompt("Ingrese la cantidad a retirar"));
+        if (
+          dinero >= dineroaretirar &&
+          !isNaN(dineroaretirar) &&
+          dineroaretirar != ""
+        ) {
+          dinero = dinero - dineroaretirar;
+          mostrardinero();
+          pedirvolver();
+        } else {
+          alert(
+            "No tiene esa cantidad en la cuenta o ingrese mal la cantidad!"
+          );
+          mostrardinero();
+          pedirvolver();
+        }
+        break;
+    }
+  }
+}
+
+function pedirvolver() {
+  let num2 = prompt("Desea volver al menu? (si/no)");
+  if (num2 == "si" || num2 == "Si" || num2 == "SI") {
+    pediropc();
+    menu();
+  } else if (num2 == "no" || num2 == "No" || num2 == "NO") {
+    opc = 4;
+  } else {
+    alert("Ingrese una respuesta valido!");
+    pedirvolver();
+  }
+}
+
+
+let dinero = 50;
+let opc;
+
+alert("Bienvenido a un cajero automatico");
+pediropc();
+menu();
+*/
+
+//2da ENTREGA
+/*
 function menu() {
   let opcionMenu = parseInt(
     prompt(
@@ -34,7 +188,7 @@ function menu() {
         Tareas.splice(parseInt(opc2) - 1, 1);
         alert("Tarea eliminada!");
         volverAlMenu();
-      } else if (opc2.toLowerCase() == "s") {
+      } else if (opc2 == "s") {
         menu();
       } else {
         alert("No existe ese numero de tarea!");
@@ -109,7 +263,7 @@ function menu() {
       break;
     default:
       error();
-      break;
+      break;  
   }
 }
 
@@ -148,3 +302,28 @@ let Tareas = [];
 
 alert("Bienvenido a su lista de tareas!");
 menu();
+*/
+
+let Tareas = [];
+
+class Tarea {
+  constructor(titulo, descripcion, dia, mes) {
+    this.titulo = titulo;
+    this.descripcion = descripcion;
+    this.dia = dia;
+    this.mes = mes;
+  }
+}
+
+let agregarTarea = document.getElementById(btn_aggTarea);
+btn_aggTarea.onclick = (e) => {
+  e.preventDefault();
+  let tituloTarea = document.getElementById(tareaTitulo);
+  let descripcionTarea = document.getElementById(tareaDesc);
+  let diaTarea = document.getElementById(tareaDia);
+  let mesTarea = document.getElementById(tareaMes);
+  let tarea = new Tarea(tituloTarea, descripcionTarea, diaTarea, mesTarea);
+  Tareas.push(tarea);
+};
+console.log(tituloTarea);
+console.log(Tareas);
